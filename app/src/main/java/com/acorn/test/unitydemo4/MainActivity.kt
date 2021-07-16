3cad4c3600d12b518998545e3bbc32169aa57aae
苏飞 <fei.su@imcore.com>
2021-07-16 17:03:58 +0800

    init

---
+++ app/src/main/java/com/acorn/test/unitydemo4/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.acorn.test.unitydemo4
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.unity3d.player.UnityPlayerActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        testTv.setOnClickListener {
+            startActivity(Intent(this,UnityPlayerActivity::class.java))
+        }
+    }
+}
